4fc9477d22afc60e9f967fc1d9a57fa1c53b3dc3
Azmain Morshed <114017420+azmainm@users.noreply.github.com>
2023-11-28 12:49:18 +0600

    Add filter and sort

--- resources/js/Pages/Bids/ViewBids.jsx
+++ resources/js/Pages/Bids/ViewBids.jsx
@@ -1,55 +1,100 @@
 // ViewBids.jsx
-import React from 'react';
-// import BidHeader from './BidHeader';
+import React, { useState } from 'react';
+import BidHeader from './BidHeader';
 import Bid from './Bid';
 
+const ViewBids = ({ bids }) => {
+  const [filterStatus, setFilterStatus] = useState('all'); // 'all', 'active', or 'notActive'
+  const [sortDirection, setSortDirection] = useState('asc'); // 'asc' or 'desc'
+  const [showStatusDropdown, setShowStatusDropdown] = useState(false);
+  const [showSortDropdown, setShowSortDropdown] = useState(false);
 
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+  // Toggle dropdowns
+  const toggleStatusDropdown = () => setShowStatusDropdown(!showStatusDropdown);
+  const toggleSortDropdown = () => setShowSortDropdown(!showSortDropdown);
 
-const ViewBids = ({ auth, status, bids }) => {
+  const getFilteredBids = () => {
+    return bids.filter(bid => {
+      if (filterStatus === 'all') return true;
+      return filterStatus === 'active' ? bid.bidStatus === "Active" : bid.bidStatus === "Not Active";
+    });
+  };
+
+  const getSortedAndFilteredBids = () => {
+    const filteredBids = getFilteredBids();
+    return filteredBids.sort((a, b) => {
+      const bidA = parseInt(a.bidAmount);
+      const bidB = parseInt(b.bidAmount);
+      if (sortDirection === 'asc') {
+        return bidA - bidB;
+      } else {
+        return bidB - bidA;
+      }
+    });
+  };
 
   return (
-      <AuthenticatedLayout
-      user={auth.user}
-      header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Bids</h2>}      
-      >
-
-      <Head title="Bids" />
-
-
-          <div className="pt-12 pb-64">
-              {/* <BidHeader /> */}
-              <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-              <div className="p-6 text-gray-900" 
-                                 style={{
-                                  'font-size':'20px',
-                                  'font-weight':'600',
-                                 }}> Bids made on your posts</div>
-
-                {bids.length > 0 ? 
-                <div> 
-                    {bids.map((bidInfo, index) => (
-                      <Bid key={index} name={bidInfo.name} bidAmount={bidInfo.bidAmount} />
-                    ))}
-                </div> 
-                  
-              : 
-                <div  className="bg-white overflow-hidden shadow-sm sm:rounded-lg" >
-                <div className="p-6 text-gray-900">No bids available 😔</div>
-               </div>
-              
-              }
-              </div>
-          </div>
-
-      </AuthenticatedLayout>
-        
-  
+    <div>
+      <BidHeader />
+      <div className="flex justify-end gap-2 mb-4 mt-4 mr-4">
+        <div className="relative">
+          <button onClick={toggleStatusDropdown} className="bg-pink-600 text-white px-4 py-2 rounded-md focus:outline-none">
+            Status
+          </button>
+          {showStatusDropdown && (
+            <div className="absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-10">
+              <a
+                href="#"
+                onClick={() => setFilterStatus('active')}
+                className={`block px-4 py-2 text-sm capitalize text-gray-700 hover:bg-pink-600 hover:text-white ${filterStatus === 'active' ? 'bg-pink-600 text-white' : ''}`}>
+                Active
+              </a>
+              <a
+                href="#"
+                onClick={() => setFilterStatus('notActive')}
+                className={`block px-4 py-2 text-sm capitalize text-gray-700 hover:bg-pink-600 hover:text-white ${filterStatus === 'notActive' ? 'bg-pink-600 text-white' : ''}`}>
+                Not Active
+              </a>
+              <a
+                href="#"
+                onClick={() => setFilterStatus('all')}
+                className={`block px-4 py-2 text-sm capitalize text-gray-700 hover:bg-pink-600 hover:text-white ${filterStatus === 'all' ? 'bg-pink-600 text-white' : ''}`}>
+                All
+              </a>
+            </div>
+          )}
+        </div>
+        <div className="relative">
+          <button onClick={toggleSortDropdown} className="bg-pink-600 text-white px-4 py-2 rounded-md focus:outline-none">
+            Sort
+          </button>
+          {showSortDropdown && (
+            <div className="absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-10">
+              <a
+                href="#"
+                onClick={() => setSortDirection('asc')}
+                className={`block px-4 py-2 text-sm capitalize text-gray-700 hover:bg-pink-600 hover:text-white ${sortDirection === 'asc' ? 'bg-pink-600 text-white' : ''}`}>
+                Ascending
+              </a>
+              <a
+                href="#"
+                onClick={() => setSortDirection('desc')}
+                className={`block px-4 py-2 text-sm capitalize text-gray-700 hover:bg-pink-600 hover:text-white ${sortDirection === 'desc' ? 'bg-pink-600 text-white' : ''}`}>
+                Descending
+              </a>
+            </div>
+          )}
+        </div>
+      </div>
+      <div>
+        {getSortedAndFilteredBids().map((bidInfo, index) => (
+          <Bid key={index} name={bidInfo.name} bidAmount={bidInfo.bidAmount} bidStatus={bidInfo.bidStatus} />
+        ))}
+      </div>
+    </div>
   );
 };
 
-// Providing default props in case bids is not passed
 ViewBids.defaultProps = {
   bids: [],
 };
